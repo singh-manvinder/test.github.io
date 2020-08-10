5d7134d92f556168202de5b1fdd2f6f35c8d1a44
Manvinder Singh <manvinder.singh60@gmail.com>
2020-08-11 02:03:50 +0530

    Updates

---
+++ precache-manifest.be152c47e0443034a4258e3929fbe1c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21ecd7bef1b76e5d4ceff08626fe317",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef068218b83481c69e62",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17cd6fec5cd3b758dc46",
+    "url": "/static/js/2.0986e7d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0986e7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef068218b83481c69e62",
+    "url": "/static/js/main.07ce0faa.chunk.js"
+  },
+  {
+    "revision": "691d9c7345d2cc5dd5a3",
+    "url": "/static/js/runtime-main.09c921d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
